90b18922bd44df702fefb6abe1e2bbdddefa9916
Fraest <kaleb.philbrick@gmail.com>
2023-09-25 21:39:40 -0400

    Character Class

---
+++ Punk Wars/Assets/Scripts/Character.cs
@@ -0,0 +1,81 @@
+private class Character
+{
+    //Variables
+    private int ID;
+    private int HP;
+    private int DEF;
+    private int ATK;
+    private int Speed;
+
+    //Constructor
+    private Character(int ID, int HP, int DEF, int ATK, int Speed)
+    {
+        this.ID = ID;
+        this.HP = HP;
+        this.DEF = DEF; 
+        this.Atk = ATK; 
+        this.Speed = Speed; 
+    }
+
+    //Getters and setters
+
+
+    //Attribute 1
+    public int GetID()
+    {
+        return ID;
+    }
+
+    public void SetID(int ID)
+    {
+        this.ID = ID;
+    }
+
+
+    //Attribute 2
+    public int GetHP()
+    {
+        return HP;
+    }
+
+    public void SetHP(int HP)
+    {
+        this.HP = HP;
+    }
+
+
+    //Attribute 3
+    public int GetDEF()
+    {
+        return DEF;
+    }
+
+    public void SetDEF(int DEF)
+    {
+        this.DEF = DEF;
+    }
+
+
+    //Attribute 4
+    public int Get ATK()
+    {
+        return ATK;
+    }
+
+    public void SetATK(int ATK)
+    {
+        this.ATK = ATK;
+    }
+
+
+    //Attribute 5
+    public int GetSpeed()
+    {
+        return Speed;
+    }
+
+    public void SetSpeed(string Speed)
+    {
+        this.Speed = Speed;
+    }
+}
