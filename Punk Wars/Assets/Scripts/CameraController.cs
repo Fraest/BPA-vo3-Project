7c82dd49b09d319914f7a2061a2454e26b8a4b92
baconman465 <ps24acolburn@efcts.us>
2023-10-02 12:42:25 -0400

    augh

--- Punk Wars/Assets/Scripts/CameraController.cs
+++ Punk Wars/Assets/Scripts/CameraController.cs
@@ -2,17 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+public class Camera : MonoBehavior
 {
-    // Start is called before the first frame update
+    public float moveSpeed;
+    public float moveTime;
+
+    public Vector3 newPos;
+
+    //Called before the first frame update
     void Start()
     {
-        
+        //stops the transform from equaling zero
+        newPos = transform.position;
     }
 
-    // Update is called once per frame
+    //Update is called once per frame
     void Update()
     {
-        
+        HandleMoveInput();
+    }
+
+    void HandleMoveInput()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            newPos += (transform.forward * moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            newPos += (transform.forward * -moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            newPos += (transform.right * -moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            newPos += (transform.right * moveSpeed);
+        }
+
+        //makes the panning smooth
+        transform.position = Vector3.Lerp(transform.position, newPos, Time.deltaTime * moveTime);
     }
 }
