91bd50840700045ff2331ac560ca777eb3d9f739
Fraest <125041013+Fraest@users.noreply.github.com>
2023-10-03 14:02:35 -0400

    Camera Parent Changes

--- Punk Wars/Assets/Scripts/CameraController.cs
+++ Punk Wars/Assets/Scripts/CameraController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera : MonoBehavior
+public class CameraController : MonoBehavior
 {
     public float moveSpeed;
     public float moveTime;
