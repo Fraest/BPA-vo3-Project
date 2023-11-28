2d00c3a1ef806d32e887c05388fb62ff7fb44e77
nam-mils <dragon19976@yahoo.com>
2023-11-27 22:00:42 -0500

    Minor bug fixes and audio adjustment

--- Punk Wars/Assets/Scripts/PlayerMovement.cs
+++ Punk Wars/Assets/Scripts/PlayerMovement.cs
@@ -10,52 +10,27 @@ public class PlayerMovement : MonoBehaviour
     private GameObject goal;
     public bool selected;
 
-    private void Start() {
+    private void Start()
+    {
         selected = false;
     }
-    
-    void Awake() {
+    void Awake()
+    {
         goal = GameObject.FindWithTag("Goal");
     }
-
     private void Update()
     {
-<<<<<<< Updated upstream
         if (Input.GetMouseButtonDown(0))
         {
-            Invoke("movePlayer", 0.1f);
-
-            if (Input.GetMouseButtonDown(1)) {
-                selected = false;
-            }
-
-            if (selected) {
-                //if raycast hits level, move player
-                //probably a better way to do this but eh
-                if (GetComponent<Camera>().GetComponent<RayCastScript>().mouseHit.collider.CompareTag("Level")) {
-                    Invoke("movePlayer", 0.1f);
-                }
-=======
-        if(Input.GetMouseButtonDown(1)){
-            selected = false;
-        }
-        //Comment stuff out for readability please 
-        if (Input.GetMouseButtonDown(0))
-        {
-            if(selected){
+            if (selected)
+            {
                 Invoke("movePlayer", 0.1f);
->>>>>>> Stashed changes
             }
         }
     }
 
-<<<<<<< Updated upstream
     private void movePlayer()
     {
-        agent.destination = goal.position;
-=======
-    private void movePlayer(){
         agent.destination = goal.transform.position;
->>>>>>> Stashed changes
     }
 }
