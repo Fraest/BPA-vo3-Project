b16342cdf718345d5b697908341953ad88f9a868
baconman465 <ps24acolburn@efcts.us>
2023-12-12 19:50:44 -0500

    i love c#!!!!!!!!!!

--- Punk Wars/Assets/Scripts/UnitBehavior.cs
+++ Punk Wars/Assets/Scripts/UnitBehavior.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using JetBrains.Annotations;
 using Unity.VisualScripting;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.UIElements;
@@ -10,25 +11,44 @@ public class UnitBehavior : MonoBehaviour
 {
     [SerializeField] NavMeshAgent agent;
     [SerializeField] Behaviour halo;
-    public bool selected, atGoal, stopped;
+    public bool selected, atGoal;
 
     private void Start() {
         selected = false;
     }
     private void Update()
     {
+        //signals if unit is selected
         if(selected) {halo.enabled = true;}
         else {halo.enabled = false;}
-        if ((this.transform.position.x-1 <= agent.destination.x) && (agent.destination.x <= this.transform.position.x +1) && (this.transform.position.y-1 <= agent.destination.y) && (agent.destination.y <= this.transform.position.y +1)){
+
+        //checks if unit is within vicinity of goal
+        //it looks bad i know shut up
+        if ((transform.position.x-1 <= agent.destination.x) && (agent.destination.x <= transform.position.x +1) && (transform.position.y-1 <= agent.destination.y) && (agent.destination.y <= transform.position.y +1)){
             atGoal = true;
         }
     }
 
     private void OnCollisionEnter(Collision other) {
-       if(other.gameObject.GetComponent<UnitBehavior>().atGoal){
-            agent.destination = this.transform.position;
+        //stops unit if its going to the same place and is at the goal
+        //yes this also looks bad, see previous comment
+        if(other.gameObject.GetComponent<UnitBehavior>().atGoal && other.gameObject.GetComponent<NavMeshAgent>().destination != agent.destination){
+            agent.destination = transform.position;
             atGoal = true;
-       }
+        }
+
+        //if unit collides with a unit that isnt going to the same place it just goes through to avoid annoyances
+        if(other.gameObject.GetComponent<NavMeshAgent>().destination != agent.destination){
+            other.gameObject.GetComponent<BoxCollider>().enabled = false;
+            Invoke("wait", 2.0f);
+            other.gameObject.GetComponent<BoxCollider>().enabled = true;
+        }
+    }
+
+
+    private void wait(){
+        //does nothing, used to just delay something using invoke
+        Debug.Log("waiting");
     }
 }
 
