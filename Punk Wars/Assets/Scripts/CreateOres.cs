a97fa965f75b93730911bf18ba79c25c84c44a6e
baconman465 <ps24acolburn@efcts.us>
2024-01-10 11:23:22 -0500

    trying to update navmesh in script

--- Punk Wars/Assets/Scripts/CreateOres.cs
+++ Punk Wars/Assets/Scripts/CreateOres.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Build.Content;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class CreateOres : MonoBehaviour
 {
+    //!!!!!!!!!!!!!!finish later
+    [SerializeField] NavMeshData navData;
+    [SerializeField] NavMeshBuildSettings buildSettings;
     // Start is called before the first frame update
     void Start()
     {
+        GameObject[] level = GameObject.FindGameObjectsWithTag("Level");
+        // NavMeshBuilder.UpdateNavMeshData(navData);
         gameObject.GetComponent<NavMeshSurface>().BuildNavMesh();
     }
 
