7a04b375646dee62e538b3c19c6e445aa610c3bf
Fraest <125041013+Fraest@users.noreply.github.com>
2024-01-11 11:51:36 -0500

    FoldersandStuff

--- Punk Wars/Assets/Scripts/Saving/SaveManager.cs
+++ Punk Wars/Assets/Scripts/Saving/SaveManager.cs
@@ -36,29 +36,8 @@
 //             {
 //                 Connection.Open();
 
-<<<<<<< Updated upstream
-//                 // set up an object command to control db
-//                 IDbCommand Command = Connection.CreateCommand();
-
-//                 // Creating the Unit Table if it doesn't already exist
-//                 Command.CommandText = "CREATE TABLE IF NOT EXISTS Main (id INTEGER, UnitHP INTEGER, UnitXPosition INTEGER, UnitYPosition INTEGER, UnitZPosition INTEGER, OnYourTeam INTEGER);";
-//                 Command.ExecuteReader();
-
-//                 Command = Connection.CreateCommand();
-
-//                 //Creating the Player Table if it doesn't already exist
-//                 Command.CommandText = "CREATE TABLE IF NOT EXISTS Player (id INTEGER, Copper INTEGER, Iron INTEGER, BaseHP);";
-                
-//                 Command = Connection.CreateCommand();
-
-//                 Connection.Close();
-//             }
-//         }
-//     }
-=======
-                // set up an object command to control db
-                IDbCommand Command = Connection.CreateCommand();
->>>>>>> Stashed changes
+// set up an object command to control db
+// IDbCommand Command = Connection.CreateCommand();
 
 //     public string Read(string table, string column, int row)
 //     {
@@ -92,42 +71,33 @@
 //         }
 //     }
 
-<<<<<<< Updated upstream
+//             return reader.GetValue(0).ToString();
+//         }
+//     }
+
+//     public void Write(string table, string column, int row, string variableToUse)
+//     {
+//         using (var connection = new SqliteConnection(dbName))
+//         {
+//             connection.Open();
+
+//             //Setting up an object command to allow db caontrol
+//             using (var command = connection.CreateCommand())
+//             {
+//                 command.CommandText = "SELECT DISTINCT " + column + " FROM " + table + " WHERE id = " + row.ToString() + " ORDER BY id;";
+//                 command.ExecuteNonQuery();
+
+//                 IDataReader dataReader = command.ExecuteReader();
+//                 command.CommandText = "UPDATE " + table + " SET " + column + " = " + variableToUse + " WHERE id = " + row.ToString();
+//                 Command.ExecuteNonQuery();
+//             }
+//             connection.Close();
+//         }
+//     }
+
 //     public void DeleteAndRecreateDatabase()
 //     {
 //         File.Delete("Database.db");
 //         CreateDB();
 //     }
 // }
-=======
-            return reader.GetValue(0).ToString();
-        }
-    }
-
-    public void Write(string table, string column, int row, string variableToUse)
-    {
-        using (var connection = new SqliteConnection(dbName))
-        {
-            connection.Open();
-
-            //Setting up an object command to allow db caontrol
-            using (var command = connection.CreateCommand())
-            {
-                command.CommandText = "SELECT DISTINCT " + column + " FROM " + table + " WHERE id = " + row.ToString() + " ORDER BY id;";
-                command.ExecuteNonQuery();
-
-                IDataReader dataReader = command.ExecuteReader();
-                command.CommandText = "UPDATE " + table + " SET " + column + " = " + variableToUse + " WHERE id = " + row.ToString();
-                Command.ExecuteNonQuery();
-            }
-            connection.Close();
-        }
-    }
-
-    public void DeleteAndRecreateDatabase()
-    {
-        File.Delete("Database.db");
-        CreateDB();
-    }
-}
->>>>>>> Stashed changes
