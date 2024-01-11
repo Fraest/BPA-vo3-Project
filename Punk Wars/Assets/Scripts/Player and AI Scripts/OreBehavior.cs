694cfb9a80027912964a0260bf247bdda30cefa6
baconman465 <ps24acolburn@efcts.us>
2024-01-11 18:47:55 -0500

    finished ore + implemented materials

--- Punk Wars/Assets/Scripts/Player and AI Scripts/OreBehavior.cs
+++ Punk Wars/Assets/Scripts/Player and AI Scripts/OreBehavior.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 public class OreBehavior : MonoBehaviour
 {
-    [SerializeField] GameObject brokenOre, oreModel;
+    [SerializeField] GameObject regularOre, brokenOre;
     public int currentHealth;
 
     void Awake() {
@@ -24,17 +24,20 @@ public class OreBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //continuously updates the current health
         currentHealth = gameObject.GetComponent<HealthManager>().health;
         if(currentHealth <= 0) {destroyed(0);}
+        //prevents health from going down if the ore is destroyed
+        if(!regularOre.activeSelf){gameObject.GetComponent<HealthManager>().health = gameObject.GetComponent<HealthManager>().maxHealth;}
     }
 
 
     public void destroyed(int ores){
         //gives a random amount of ores
         ores += Random.Range(1,3);
-        //instantiates broken ore with regular ore as the parent
-        Instantiate(brokenOre, gameObject.transform);
-        //deactivates the regular ore
-        oreModel.SetActive(false);
+        //deactivates the regular ore and activates the broken ore
+        brokenOre.SetActive(true);
+        //resets health for when it regens
+        regularOre.SetActive(false);
     }
 }
