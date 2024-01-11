9713fac8bd5233927f9862e1220671f3776479c6
nam-mils <dragon19976@yahoo.com>
2024-01-10 21:21:33 -0500

    Lots of menu changes

--- Punk Wars/Assets/Scripts/Menu1.cs
+++ Punk Wars/Assets/Scripts/Menu1.cs
@@ -9,6 +9,18 @@ public class Menu1 : MonoBehaviour
     //Increases build index by 1 to go to the next scene in the index
     public void Play()
     {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    //Sends you to the main menu or Scene 0
+    public void Menu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    //Sends you to the Tutorial
+    public void Tutorial()
+    {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
@@ -18,4 +30,6 @@ public class Menu1 : MonoBehaviour
         Application.Quit();
         Debug.Log("QUIT");
     }
+
+    
 }
---
+++ Punk Wars/Assets/Scripts/Pause.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    public static bool GamePaused = false;
+    public GameObject pauseMenuUI;
+    //When user presses Esc checks if game is paused or not then runs the script
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GamePaused){
+                Resume();
+            }
+            else{
+                Paused();
+            }
+        }
+    }
+
+    void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GamePaused = false; 
+    }
+
+    void Paused()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GamePaused = true; 
+    }
+}
