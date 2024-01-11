8a4d6d57f01c5aa195ae9ce0492f763c5d902da6
nam-mils <dragon19976@yahoo.com>
2024-01-11 08:59:51 -0500

    Error Handling and Crash Report and Logging

--- Punk Wars/Assets/Scripts/Pause.cs
+++ Punk Wars/Assets/Scripts/Pause.cs
@@ -6,6 +6,7 @@ public class Pause : MonoBehaviour
 {
     public static bool GamePaused = false;
     public GameObject pauseMenuUI;
+    
     //When user presses Esc checks if game is paused or not then runs the script
     void Update()
     {
@@ -32,5 +33,19 @@ public class Pause : MonoBehaviour
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GamePaused = true; 
+    }
+
+    // pauses game on crash
+     public void PauseCrashed()
+    {
+        GamePaused = true; 
+        Time.timeScale = 0f; 
+    }
+
+    //resumes game after crash
+    public void ResumeCrashed() 
+    {
+        GamePaused = false; 
+        Time.timeScale = 1f;
     }
 }
