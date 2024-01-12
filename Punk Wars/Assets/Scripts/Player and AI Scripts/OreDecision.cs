bcb3ed26fa3f36f14d0868e203d95b1231e4551e
baconman465 <ps24acolburn@efcts.us>
2024-01-11 20:11:38 -0500

    yippee!!!!

--- Punk Wars/Assets/Scripts/Player and AI Scripts/OreDecision.cs
+++ Punk Wars/Assets/Scripts/Player and AI Scripts/OreDecision.cs
@@ -7,7 +7,10 @@ public class OreDecision : MonoBehaviour
     public bool isCopper;
     void Awake() {
         //randomly decides if the ore is copper or iron
-        if(Random.Range(0,2) == 0){isCopper = false;}
+        if(Random.Range(0,2) == 0){
+            isCopper = false;
+            
+        }
         else{isCopper = true;}
         Debug.Log(isCopper);
     }
